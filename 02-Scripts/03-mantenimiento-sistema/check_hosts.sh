be6ba256b4f7ab732e8996780cf2b777b8804563
Omar David Conde Suarez <omarsuarez988@gmail.com>
2026-08-03 10:13:21 -0600

    Se agregaron dos scripts en la carpeta 01 en el apartado 03 matenimiento del sistema

---
+++ 02-Scripts/03-mantenimiento-sistema/check_hosts.sh
@@ -0,0 +1,18 @@
+ #!/bin/bash 
+STATIC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
+FILE="$STATIC_DIR/datos_prueba_mantenimiento/hosts.txt"
+if [ ! -f "$FILE" ]; then 
+        echo "EL archivo no existe" >&2
+        exit 1
+fi
+
+
+while read -r hosts ; do 
+        ping -c 1 -W 2 "$hosts" > /dev/null
+        if [ $? -eq 0 ];then
+                echo "[ONLINE] $hosts"
+        else 
+
+                echo "[OFFILE] $hosts"
+        fi
+done < "$FILE"
